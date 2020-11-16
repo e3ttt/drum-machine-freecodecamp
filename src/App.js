92e62e8c34de492ad266529c67ce733e79ff8b1c
e3ttt <trindade3e@gmail.com>
2020-11-16 16:50:01 -0300

    Added drum kit sounds

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="drum-machine">
+      <div id="display">Display</div>
+      <div className="drum-pad-container">
+        <div className="drum-pad" id="bass-drum">
+          <audio id="Q" className="clip" src=""></audio>Q
+        </div>
+        <div className="drum-pad" id="floor-tom">
+          <audio id="W" className="clip" src=""></audio>W
+        </div>
+        <div className="drum-pad" id="snare-drum">
+          <audio id="E" className="clip" src=""></audio>E
+        </div>
+        <div className="drum-pad" id="tom-tom">
+          <audio id="A" className="clip" src=""></audio>A
+        </div>
+        <div className="drum-pad" id="hi-hat">
+          <audio id="S" className="clip" src=""></audio>S
+        </div>
+        <div className="drum-pad" id="crash-cymbal">
+          <audio id="D" className="clip" src=""></audio>D
+        </div>
+        <div className="drum-pad" id="ride-cymbal">
+          <audio id="Z" className="clip" src=""></audio>Z
+        </div>
+        <div className="drum-pad" id="splash-cymbal">
+          <audio id="X" className="clip" src=""></audio>X
+        </div>
+        <div className="drum-pad" id="china-cymbal">
+          <audio id="C" className="clip" src=""></audio>C
+        </div>
+      </div>
     </div>
   );
 }
